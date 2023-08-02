6f52f4a27eec26112656b351a20ae3d66de7a1a7
Francisco J. Aguaisol <faguaisol@redsos.com.ar>
2023-08-02 14:32:44 -0300

    NavBar Estilo

--- Client/src/Components/Home/NavBar/NavBarHome.jsx
+++ Client/src/Components/Home/NavBar/NavBarHome.jsx
@@ -6,18 +6,17 @@ export default function NavBarHome() {
 
   return (
     
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+<nav className='conteiner' class="navbar navbar-expand-lg bg-body-tertiary">
+<img className='logo' src={Logo} alt="" />
+  <div className='lista' class="container-fluid">
+
+
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link" href="#">Noticias</a>
-        <a class="nav-link" href="#">Reintegro</a>
-        <a class="nav-link" href="#">Contacto</a>
-        <a class="nav-link" href="#">Informacion</a>
+      <div className='seccion' class="navbar-nav">
+        <a class="nav-link" href="Client/src/Home/Secciones/Noticias">NOTICIAS</a>
+        <a class="nav-link" href="Client/src/Home/Secciones/Reintegro">REINTEGRO</a>
+        <a class="nav-link" href="Client/src/Home/Secciones/Agenda">AGENDA</a>
+        <a class="nav-link" href="Client/src/Home/Secciones/Informacion">INFORMACION</a>
       </div>
     </div>
   </div>
