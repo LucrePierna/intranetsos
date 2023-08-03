dcf34a232d34f1eff45f4146405f0090f562ceaf
LucasRibotta <lucasribotta15@gmail.com>
2023-08-02 23:34:33 -0300

    fix data

--- Client/src/Components/Home/NavBar/NavBarHome.jsx
+++ Client/src/Components/Home/NavBar/NavBarHome.jsx
@@ -6,17 +6,6 @@ export default function NavBarHome() {
 
   return (
     
-<<<<<<< HEAD
-    <nav class='conteiner'>
-      <img class='logo' src={Logo} alt="" />
-      <div class='lista'>
-      <ul class='seccion'>
-        <li>Noticias</li>
-        <li>Reintegro</li>
-        <li>Contacto</li>
-        <li>Informacion</li>
-      </ul>
-=======
 <nav className='conteiner' class="navbar navbar-expand-lg bg-body-tertiary">
 <img className='logo' src={Logo} alt="" />
   <div className='lista' class="container-fluid">
@@ -28,7 +17,6 @@ export default function NavBarHome() {
         <a class="nav-link" href="Client/src/Home/Secciones/Reintegro">REINTEGRO</a>
         <a class="nav-link" href="Client/src/Home/Secciones/Agenda">AGENDA</a>
         <a class="nav-link" href="Client/src/Home/Secciones/Informacion">INFORMACION</a>
->>>>>>> d426eb068b8b20f9d17f8151737b64acf65b45aa
       </div>
     </div>
   </div>
