b1d1b958ea94cda772b92f09c9a985485b454e8d
Lucre <lpierna@redsos.com.ar>
2023-08-16 13:30:11 -0300

    cambios

--- Client/src/Components/Tareas/TerminalesYPesados/Secciones/Mapas/Mapas.jsx
+++ Client/src/Components/Tareas/TerminalesYPesados/Secciones/Mapas/Mapas.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Mapas() {
   return (
-    <div>Mapas</div>
+    <div>
+      <h1>Mapa</h1>
+      <iframe src="https://www.google.com/maps/d/embed?mid=1A5W5cPrDYXenERKan34PXlGJSg__vhQ&ehbc=2E312F" width="640" height="480"></iframe>
+    </div>
+    
   )
 }
--- Client/src/Components/Tareas/TraficoNacional/Secciones/Mapas/Mapas.jsx
+++ Client/src/Components/Tareas/TraficoNacional/Secciones/Mapas/Mapas.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Mapas() {
   return (
-    <div>Mapas</div>
+    <div>
+      <h1>Mapas</h1>
+      <iframe src="https://www.google.com/maps/d/embed?mid=15M-f4mQAl7_mxWow8vt-Uh0v71dUDrc&ehbc=2E312F" width="640" height="480"></iframe>
+    </div>
   )
 }
