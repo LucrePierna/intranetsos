48bada18d8714973e19fa4fb182655c0beb3ece6
LucasRibotta <lucasribotta15@gmail.com>
2023-08-16 13:52:42 -0300

    cambio url link viajero flugograma

--- Client/src/Components/Tareas/Viajeros/Secciones/Flujograma/Flujograma.jsx
+++ Client/src/Components/Tareas/Viajeros/Secciones/Flujograma/Flujograma.jsx
@@ -6,12 +6,12 @@ export default function Flujograma() {
     <div className='d-flex flex-column align-items-center justify-content-center mx-auto  mt-5'>
       <div>
         <iframe
-          src="https://redsos.sharepoint.com/:u:/r/sites/intranet.rednacional/_layouts/15/Doc.aspx?sourcedoc=%7B1c70c4c5-7b01-4de8-93a5-03ce0ecfc6d6%7D&amp=&action=embedview" width="800px" height="500px" frameborder="0">Esto es un documento de
+          src="https://redsos-my.sharepoint.com/:u:/r/personal/lugomez_redsos_com_ar/_layouts/15/Doc.aspx?sourcedoc=%7Bc3ef6e1d-bb25-4c3e-96c4-d26e38228643%7D&amp=&action=embedview" width="800px" height="500px" frameborder="0">Esto es un documento de
           <a target="_blank" href="https://office.com" rel="noreferrer">Microsoft Office</a> incrustado con tecnología de
           <a target="_blank" href="https://office.com/webapps" rel="noreferrer">Office</a>.</iframe>
       </div>
       <div className='mt-3'>
-        <button> <a target='blank' href="https://redsos.sharepoint.com/:u:/r/sites/intranet.rednacional/_layouts/15/Doc.aspx?sourcedoc=%7B1c70c4c5-7b01-4de8-93a5-03ce0ecfc6d6%7D&amp=&action=embedview">Vista</a></button>
+        <button> <a target='blank' href="https://redsos-my.sharepoint.com/:u:/r/personal/lugomez_redsos_com_ar/_layouts/15/Doc.aspx?sourcedoc=%7Bc3ef6e1d-bb25-4c3e-96c4-d26e38228643%7D&amp=&action=embedview">Vista</a></button>
       </div>
     </div>
   )
