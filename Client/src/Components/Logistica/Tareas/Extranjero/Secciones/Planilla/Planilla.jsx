2ce7c0b0f5e0ecfa6fabe0ad9ab2c68d07d40578
Francisco J. Aguaisol <faguaisol@redsos.com.ar>
2023-08-19 14:38:32 -0300

    planillas

--- Client/src/Components/Logistica/Tareas/Extranjero/Secciones/Planilla/Planilla.jsx
+++ Client/src/Components/Logistica/Tareas/Extranjero/Secciones/Planilla/Planilla.jsx
@@ -9,9 +9,9 @@ export default function Planilla() {
      {/*  <img className='img' src={ImgPlanilla} alt="planilla" /> */}
      <iframe src="https://redsos-my.sharepoint.com/:x:/r/personal/lpierna_redsos_com_ar/_layouts/15/Doc.aspx?sourcedoc=%7B865D5B4F-42CC-47FF-9C63-9815123182A0%7D&file=Extranjeros.xlsx&action=default&mobileredirect=true" 
       width="640" height="480"></iframe>
-      <button><a
-        href="https://redsos-my.sharepoint.com/:x:/r/personal/lpierna_redsos_com_ar/Documents/Extranjeros.xlsx?d=w865d5b4f42cc47ff9c639815123182a0&csf=1&web=1&e=IyKZWu"
-        target='blank'>Acceso</a></button>
+      <a href="https://redsos-my.sharepoint.com/:x:/r/personal/lpierna_redsos_com_ar/Documents/Extranjeros.xlsx?d=w865d5b4f42cc47ff9c639815123182a0&csf=1&web=1&e=IyKZWu" target="_blank" rel="noopener noreferrer">
+        <button type="button" class="btn btn-outline-light">Planilla Extranjero</button>
+        </a>
     </div>
   )
 }
--- Client/src/Components/Logistica/Tareas/Transportin/Secciones/Planillas/Planillas.jsx
+++ Client/src/Components/Logistica/Tareas/Transportin/Secciones/Planillas/Planillas.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import ImgPlanilla from '../../../../Image/PlanillaTransportin.png'
+import './Planillas.css'
 
-export default function Planillas() {
+
+export default function Planilla() {
   return (
-    <div>Planillas</div>
+    <div className='d-flex flex-column align-items-center justify-content-center mx-auto  mt-5'>
+      <h1>Planilla Transportin</h1>
+     { <img className='img' src={ImgPlanilla} alt="planilla" /> }
+        <a href="https://redsos.sharepoint.com/:x:/r/sites/transportincba/Shared%20Documents/Datos%20adjuntos%20de%20correo%20electr%C3%B3nico/Transportin%202023%20Final.xlsx?d=wb9b66c3dce8446b19467e54ee7c4d521&csf=1&web=1&e=0mUNeB" target="_blank" rel="noopener noreferrer">
+        <button type="button" class="btn btn-outline-light">Planilla Transportin</button>
+        </a>
+        <a href="https://redsos-my.sharepoint.com/:x:/r/personal/sheredia_redsos_com_ar/Documents/resguardo%20sheredia/ORGANIZACION%20ENTREGAS%20TRANSPORTIN%206001%20-2023.xlsx?d=wd354a53169e9487fba8437f4330756ae&csf=1&web=1&e=nPJCTH" target="_blank" rel="noopener noreferrer">
+        <button type="button" class="btn btn-outline-light">Entregas Transportin</button>
+        </a>
+    </div>
   )
 }
