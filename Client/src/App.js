ce60d5f1d8e5183f6d4e23e8155c0f5a05dc3ca9
LucasRibotta <lucasribotta15@gmail.com>
2023-08-19 09:46:22 -0300

    fix estructura data + mapas de transportin

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Menu from './Menu/Menu';
-import NavBarHome from "./Components/Home/NavBar/NavBarHome";
-import NavBarDerivacion from "./Components/Tareas/Derivacion/Navbar/NavBarDerivacion";
-import NavBarTrafico from "./Components/Tareas/TraficoNacional/NavBar/NavBarTrafico";
-import NavBarTransportin from "./Components/Tareas/Transportin/NavBar/NavBarTransportin";
-import NavBarAtp from "./Components/Tareas/Atp/NavBar/NavBarAtp";
-import NavBarTdc from "./Components/Tareas/TDC/NavBar/NavBarTdc";
-import NavBarViajero from "./Components/Tareas/Viajeros/NavBar/NavBarViajero";
-import NavBarTP from "./Components/Tareas/TerminalesYPesados/NavBar/NavBarTP";
-import NavBarExtranjero from "./Components/Tareas/Extranjero/NavBar/NavBarExtranjero";
+import Menu from './Components/Logistica/Menu/Menu';
+import NavBarHome from "./Components/Logistica/Home/NavBar/NavBarHome";
+import NavBarDerivacion from "./Components/Logistica/Tareas/Derivacion/Navbar/NavBarDerivacion";
+import NavBarTrafico from "./Components/Logistica/Tareas/TraficoNacional/NavBar/NavBarTrafico";
+import NavBarTransportin from "./Components/Logistica/Tareas/Transportin/NavBar/NavBarTransportin";
+import NavBarAtp from "./Components/Logistica/Tareas/Atp/NavBar/NavBarAtp";
+import NavBarTdc from "./Components/Logistica/Tareas/TDC/NavBar/NavBarTdc";
+import NavBarViajero from "./Components/Logistica/Tareas/Viajeros/NavBar/NavBarViajero";
+import NavBarTP from "./Components/Logistica/Tareas/TerminalesYPesados/NavBar/NavBarTP";
+import NavBarExtranjero from "./Components/Logistica/Tareas/Extranjero/NavBar/NavBarExtranjero";
 import Landing from "./Components/Landing/Landing"; 
 
 
--- Client/src/Components/Logistica/Tareas/Extranjero/Secciones/Planilla/Planilla.jsx
+++ Client/src/Components/Logistica/Tareas/Extranjero/Secciones/Planilla/Planilla.jsx
@@ -6,7 +6,9 @@ export default function Planilla() {
   return (
     <div className='d-flex flex-column align-items-center justify-content-center mx-auto  mt-5'>
       <h1>Planilla Extranjero</h1>
-      <img className='img' src={ImgPlanilla} alt="planilla" />
+     {/*  <img className='img' src={ImgPlanilla} alt="planilla" /> */}
+     <iframe src="https://redsos-my.sharepoint.com/:x:/r/personal/lpierna_redsos_com_ar/_layouts/15/Doc.aspx?sourcedoc=%7B865D5B4F-42CC-47FF-9C63-9815123182A0%7D&file=Extranjeros.xlsx&action=default&mobileredirect=true" 
+      width="640" height="480"></iframe>
       <button><a
         href="https://redsos-my.sharepoint.com/:x:/r/personal/lpierna_redsos_com_ar/Documents/Extranjeros.xlsx?d=w865d5b4f42cc47ff9c639815123182a0&csf=1&web=1&e=IyKZWu"
         target='blank'>Acceso</a></button>
