48174dc95b427877cc36dc41aee3d46935616650
LucasRibotta <lucasribotta15@gmail.com>
2023-08-23 10:28:18 -0300

    fix data

--- Client/src/Components/Landing/Landing.jsx
+++ Client/src/Components/Landing/Landing.jsx
@@ -3,22 +3,24 @@ import React from 'react'
 
 export default function Landing() {
   return (
-    <div className='d-flex flex-column justify-content-center m-auto mt-20%'>
+    <div >
+      <div className='d-flex flex-column justify-content-center'>
 
-      <h1>Operaciones SOS</h1>
+        <h1 >Operaciones SOS</h1>
 
-      <div className='d-flex flex-row justify-content-evenly mt-5'>
-        <a href="/logistica">
-          <button type="button" className="btn-logistica">Logística</button>
-        </a>
+        <div className='d-flex flex-row justify-content-evenly mt-5'>
+          <a href="/logistica">
+            <button type="button" className="btn btn-outline-danger">LOGISTICA</button>
+          </a>
 
-        <a href="/trafico">
-          <button type="button" className="btn btn-outline-danger">Tráfico</button>
-        </a>
+          <a href="/trafico">
+            <button type="button" className="btn btn-outline-danger">TRAFICO</button>
+          </a>
 
-        <a href="/cat">
-          <button type="button" className="btn btn-outline-danger">Cat</button>
-        </a>
+          <a href="/cat">
+            <button type="button" className="btn btn-outline-danger">CAT</button>
+          </a>
+        </div>
       </div>
     </div>
   )
