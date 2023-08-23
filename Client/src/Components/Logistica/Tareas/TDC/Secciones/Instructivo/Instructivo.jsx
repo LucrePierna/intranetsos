26b1c290a4ab33f8c6e57a8af12e06f4dfd478b8
LucasRibotta <lucasribotta15@gmail.com>
2023-08-23 13:48:37 -0300

    fix data estructura de tdc

--- Client/src/Components/Logistica/Tareas/TDC/Secciones/Instructivo/Instructivo.jsx
+++ Client/src/Components/Logistica/Tareas/TDC/Secciones/Instructivo/Instructivo.jsx
@@ -1,7 +1,47 @@
 import React from 'react'
+import * as bootstrap from 'bootstrap'
 
 export default function Instructivo() {
+  const triggerTabList = document.querySelectorAll('#myTab button')
+  triggerTabList.forEach(triggerEl => {
+    const tabTrigger = new bootstrap.Tab(triggerEl)
+
+    triggerEl.addEventListener('click', event => {
+      event.preventDefault()
+      tabTrigger.show()
+    })
+  })
   return (
-    <div>Instructivo</div>
+    <div className='d-flex flex-column align-items-center justify-content-center mx-auto  mt-5'>
+      <nav>
+        <div className="nav nav-tabs" id="nav-tab" role="tablist">
+          <button className="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Descripcion de la tarea</button>
+          <button className="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Conexion eficaz</button>
+          <button className="nav-link" id="nav-cierra-tab" data-bs-toggle="tab" data-bs-target="#nav-vehicles" type="button" role="tab" aria-controls="nav-vehicles" aria-selected="false">Formato de registro</button>
+          <button className="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Motivos de rechazo</button>
+        </div>
+      </nav>
+      <div className="tab-content" id="nav-tabContent">
+        <div className="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">
+          <h1>Descripcion de la tarea</h1>
+          <ul>
+            <li>Visualización y análisis de la pantallas para la derivacion de servicios.</li>
+            <li>Analizar las zonas en donde finalizan los servicios.</li>
+            <li>Corroborar que sea conveniente la triangulación de los servicios</li>
+            <li>Ofrecer a los prestadores la posibilidad de asistir a los clientes a través de este “enganche” de servicios.</li>
+          </ul>
+
+        </div>
+        <div className="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab" tabindex="0">
+          <h1>Conexion eficaz</h1>
+        </div>
+        <div className="tab-pane fade" id="nav-vehicles" role="tabpanel" aria-labelledby="nav-cierra-tab" tabIndex="0">
+          <h1>Formato de registro</h1>
+        </div>
+        <div className="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">
+          <h1>Motivos de rechazo</h1>
+        </div>
+      </div>
+    </div>
   )
 }
--- Client/src/Components/Logistica/Tareas/TDC/Secciones/Planilla/Planilla.jsx
+++ Client/src/Components/Logistica/Tareas/TDC/Secciones/Planilla/Planilla.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import ImgPlanillatdc from '../../../../Image/planilla-tdc.png'
 
 export default function Planilla() {
   return (
-    <div>Planilla</div>
+    <div className='d-flex flex-column align-items-center justify-content-center mx-auto  mt-5'>
+    <h1>Torre de Conexion</h1>
+    <img className='img' src={ImgPlanillatdc} alt="planilla" /> 
+    <a href="https://redsos-my.sharepoint.com/:x:/r/personal/lpierna_redsos_com_ar/_layouts/15/Doc.aspx?sourcedoc=%7B5421D102-2397-4907-B6DB-5BD402591161%7D&file=REDUCCI%25u00d3N%20DE%20COSTOS%202023.xlsx&action=default&mobileredirect=true" target="_blank" rel="noopener noreferrer">
+      <button type="button" class="btn btn-outline-light mt-3">Planilla Torre de conexion</button>
+      </a>
+  </div>
   )
 }
