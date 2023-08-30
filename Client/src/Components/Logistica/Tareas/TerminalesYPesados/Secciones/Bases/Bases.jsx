e1123f0351ca80c0a6dfc5bf8b5770ff073a7d5f
Lucre <lpierna@redsos.com.ar>
2023-08-30 14:02:10 -0300

    modificacion Lucre i

--- Client/src/Components/Logistica/Tareas/TerminalesYPesados/Secciones/Bases/Bases.jsx
+++ Client/src/Components/Logistica/Tareas/TerminalesYPesados/Secciones/Bases/Bases.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import ImgPlanillaPesados from '../../../../Image/PlanillaBaseDePesados.png'
 
-export default function Bases() {
+export default function Planilla() {
   return (
-    <div>Bases</div>
+    <div className='d-flex flex-column align-items-center justify-content-center mx-auto  mt-5'>
+      <h1>Planilla de bases de pesados</h1>
+      <img className='img' src={ImgPlanillaPesados} alt="planilla" /> 
+      <a href="https://redsos-my.sharepoint.com/:x:/r/personal/lpierna_redsos_com_ar/Documents/Bases%20de%20pesados.xlsx?d=wa06ab9e22ee94df5a42630b6d4faf7ca&csf=1&web=1&e=OybvVu">
+        <button type="button" class="btn btn-outline-light">Planilla de bases de pesados</button>
+        </a>
+    </div>
   )
 }
