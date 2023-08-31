ccb138f7cbfbf4cf3f171a46d64413e6b2574c89
LucasRibotta <lucasribotta15@gmail.com>
2023-08-31 09:49:53 -0300

    fix date

--- Client/src/Components/Logistica/Tareas/Viajeros/Secciones/Links/Links.jsx
+++ Client/src/Components/Logistica/Tareas/Viajeros/Secciones/Links/Links.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
+
 export default function Links() {
   return (
-    <div>Links</div>
+    <div className='d-flex flex-column py-4 m-auto mt-5'>
+      <a href="https://www.centraldepasajes.com.ar" target='_blank'><button type="button" class="btn btn-danger my-2">Central de Pasajes</button></a>
+      <a href="https://www.plataforma10.com.ar" target='_blank'><button type="button" class="btn btn-danger my-2">Plataforma 10</button></a>
+      <a href="https://www.telexplorer.com.ar/" target='_blank'><button type="button" class="btn btn-danger my-2">Telexplorer</button></a>
+    </div>
   )
 }
