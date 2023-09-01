febdc6043bc54d794141612062add3f2dd6e4e2c
LucasRibotta <lucasribotta15@gmail.com>
2023-09-01 12:39:18 -0300

    cambios

--- Client/src/Components/Logistica/Tareas/Viajeros/Secciones/Calculadora/Calculadora.jsx
+++ Client/src/Components/Logistica/Tareas/Viajeros/Secciones/Calculadora/Calculadora.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
+import Formato from './Formato';
 
-export default function Calculadora() {
+export default function Calculadora({resultado}) {
+
 
   const [formData, setFormData] = useState({
     cantidadTotal: '',
@@ -12,7 +14,7 @@ export default function Calculadora() {
     observaciones: ''
   });
   const [copied, setCopied] = useState(false);
-  
+
   const handleFieldChange = (field, value) => {
     setFormData(prevData => ({
       ...prevData,
@@ -37,72 +39,75 @@ export default function Calculadora() {
   };
 
   return (
-    <div>
-      <div className="tab-pane fade show active" id="nav-copy" role="tabpanel" aria-labelledby="nav-copy-tab" tabindex="0">
-          <div className='d-flex flex-column mt-5 mx-auto justify-content-center text-start'>
-            <div>
-              <label className="px-3">Cantidad total de personas:</label>
-              <input
-                type="number"
-                value={formData.cantidadTotal}
-                onChange={e => handleFieldChange('cantidadTotal', e.target.value)}
-              />
-            </div>
-            <div>
-              <label className="px-3">Cantidad de personas por asistencia al viajero:</label>
-              <input
-                type="number"
-                value={formData.cantidadPorAsistencia}
-                onChange={e => handleFieldChange('cantidadPorAsistencia', e.target.value)}
-              />
-            </div>
-            <div>
-              <label className="px-3 pt-2">Origen de las personas:</label>
-              <input
-                type="text"
-                value={formData.origen}
-                onChange={e => handleFieldChange('origen', e.target.value)}
-              />
-            </div>
-            <div>
-              <label className="px-3 pt-2">Destino de las personas:</label>
-              <input
-                type="text"
-                value={formData.destino}
-                onChange={e => handleFieldChange('destino', e.target.value)}
-              />
-            </div>
-            <div>
-              <label className="px-3 pt-2">Tipo de Asistencia:
-                <input
-                  type="number"
-                  value={formData.tipoAsistencia}
-                  onChange={e => handleFieldChange('tipoAsistencia', e.target.value)}
-                />
-                KM </label>
-            </div>
-            <div>
-              <label className="px-3 pt-2">Tope establecido por la CIA:</label>
-              <input
-                type="text"
-                value={formData.topeCIA}
-                onChange={e => handleFieldChange('topeCIA', e.target.value)}
-              />
-            </div>
-            <div>
-              <label className="px-3 pt-2">Observaciones:</label>
-              <input
-                type="text"
-                value={formData.observaciones}
-                onChange={e => handleFieldChange('observaciones', e.target.value)}
-              />
-            </div>
+    <div className='d-flex flex-row h-100 justify-content-center align-items-center'>
+      <div className='d-flex px-5 mt-5 justify-content-center'>
+        <Formato />
+      </div>
 
-            <button class="btn btn-dark mt-3 h-50 w-50 m-auto" onClick={handleCopyClick}>
-              {copied ? 'Copiado' : 'Copiar'}
-            </button>
-          </div>
+      <div className='d-flex flex-column mt-5 justify-content-center text-start'>
+        <div>
+          <label className="px-3">Cantidad total de personas:</label>
+          <input
+            type="number"
+            value={formData.cantidadTotal}
+            onChange={e => handleFieldChange('cantidadTotal', e.target.value)}
+          />
         </div>
+        <div>
+          <label className="px-3">Cantidad de personas por asistencia al viajero:</label>
+          <input
+            type="number"
+            value={formData.cantidadPorAsistencia}
+            onChange={e => handleFieldChange('cantidadPorAsistencia', e.target.value)}
+          />
+        </div>
+        <div>
+          <label className="px-3 pt-2">Origen de las personas:</label>
+          <input
+            type="text"
+            value={formData.origen}
+            onChange={e => handleFieldChange('origen', e.target.value)}
+          />
+        </div>
+        <div>
+          <label className="px-3 pt-2">Destino de las personas:</label>
+          <input
+            type="text"
+            value={formData.destino}
+            onChange={e => handleFieldChange('destino', e.target.value)}
+          />
+        </div>
+        <div>
+          <label className="px-3 pt-2">Tipo de Asistencia:
+            <input
+              type="number"
+              value={formData.tipoAsistencia}
+              onChange={e => handleFieldChange('tipoAsistencia', e.target.value)}
+            />
+            KM </label>
+        </div>
+        <div>
+          <label className="px-3 pt-2">Tope establecido por la CIA:</label>
+          <input
+            type="text"
+            value={formData.topeCIA}
+            onChange={e => handleFieldChange('topeCIA', e.target.value)}
+          />
+        </div>
+        <div>
+          <label className="px-3 pt-2">Observaciones:</label>
+          <input
+            type="text"
+            value={formData.observaciones}
+            onChange={e => handleFieldChange('observaciones', e.target.value)}
+          />
+        </div>
+        <div className='pt-3 m-auto h-50 w-50 m-auto'>
+          <button className="btn btn-dark mt-3 " onClick={handleCopyClick}>
+            {copied ? 'Copiado' : 'Copiar'}
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
