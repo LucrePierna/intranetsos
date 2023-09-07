a8a5a186b81e323b4f5a81afe439c601386864f9
Francisco J. Aguaisol <faguaisol@redsos.com.ar>
2023-09-07 13:16:40 -0300

    transportin y Terminales

--- Client/src/Components/Logistica/Tareas/Transportin/Secciones/Bases/Bases.jsx
+++ Client/src/Components/Logistica/Tareas/Transportin/Secciones/Bases/Bases.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import ImgPlanilla from '../../../../Image/BaseGuardaTransportin.png'
+import './Bases.css'
 
-export default function Bases() {
+
+export default function Planilla() {
   return (
-    <div>Bases</div>
+    <div className='d-flex flex-column align-items-center justify-content-center mx-auto  mt-5'>
+      <h1>Bases de Guarda</h1>
+      {<img className='img' src={ImgPlanilla} alt="planilla" />}
+      <div className='d-grid g-col-2 gap-1 col-2 '>
+        <a href="https://redsos-my.sharepoint.com/:x:/r/personal/lugomez_redsos_com_ar/Documents/PRESTADORES%20TRANSPORTIN.xlsx?d=w278b4127b8954618aa1b2f6ee37a1d0f&csf=1&web=1&e=ogxaMj" target="_blank" rel="noopener noreferrer">
+          <button type="button" class="btn btn-light">Bases de Guarda Transportin</button>
+        </a>
+      </div>
+    </div>
   )
 }
