ebccbc5c8ad1ca5fae36147a16ed79e87c3de88f
LucasRibotta <lucasribotta15@gmail.com>
2023-09-14 14:38:10 -0300

    update data menu

--- Client/src/Components/Logistica/Menu/MenuLogistica.jsx
+++ Client/src/Components/Logistica/Menu/MenuLogistica.jsx
@@ -151,14 +151,14 @@ const MenuLogistica = () => {
           <li>
             <a
               href="/"
-              className={selectedRoute === '/' ? 'active' : ''}
+              /* className={selectedRoute === '/' ? 'active' : ''} */
               onClick={() => handleRouteClick('/')}
+              className='fst-italic separateLine'
             >
-              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-box-arrow-left mx-2" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z" />
-                <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z" />
+              <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-arrow-left-circle-fill mx-2" viewBox="0 0 16 16">
+                <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
               </svg>
-              Menu Principal
+              Volver a Operaciones
             </a>
           </li>
         </ul>
