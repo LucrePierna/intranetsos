f2ac1108fbb1b474a7c84fb45af3dff53e09091f
LucasRibotta <lucasribotta15@gmail.com>
2023-09-15 13:48:34 -0300

    update flujograma style

--- Client/src/Components/Logistica/Tareas/Extranjero/Secciones/Flujograma/Flujograma.jsx
+++ Client/src/Components/Logistica/Tareas/Extranjero/Secciones/Flujograma/Flujograma.jsx
@@ -2,33 +2,13 @@ import React from 'react';
 
 export default function Flujograma() {
   return (
-    <div className='container-flujograma'>
-      <div className="embed-responsive embed-responsive-16by9">
-        <iframe src="https://redsos-my.sharepoint.com/:u:/r/personal/pvivas_redsos_com_ar/_layouts/15/Doc.aspx?sourcedoc=%7B62bf8183-24d5-4395-847e-56b0caaa2879%7D&amp=&action=embedview"
-          className="embed-responsive-item"
-          frameborder="0"
-          title="Flujograma"
-        >
-          Esto es un documento de{' '}
-          <a target="_blank" href="https://office.com" rel="noreferrer">
-            Microsoft Office
-          </a>{' '}
-          incrustado con tecnología de{' '}
-          <a target="_blank" href="https://office.com/webapps" rel="noreferrer">
-            Office
-          </a>
-          .
-        </iframe>
-      </div>
+    <div className='conteinerFlujograma'>
+      <iframe
+        src="https://redsos-my.sharepoint.com/:u:/r/personal/pvivas_redsos_com_ar/_layouts/15/Doc.aspx?sourcedoc=%7B62bf8183-24d5-4395-847e-56b0caaa2879%7D&amp=&action=embedview" width="1280px" height="700px" frameborder="0">Esto es un documento de
+        <a target="_blank" href="https://office.com" rel="noreferrer">Microsoft Office</a> incrustado con tecnología de
+        <a target="_blank" href="https://office.com/webapps" rel="noreferrer">Office</a>.</iframe>
       <div className='mt-3'>
-        <a
-          target='blank'
-          href="https://redsos-my.sharepoint.com/:u:/r/personal/pvivas_redsos_com_ar/_layouts/15/Doc.aspx?sourcedoc=%7B62bf8183-24d5-4395-847e-56b0caaa2879%7D&amp=&action=embedview"
-        >
-          <button type='button' className="btn btn-danger mt-3">
-            Vista
-          </button>
-        </a>
+      <a target='blank' href="https://redsos-my.sharepoint.com/:u:/r/personal/pvivas_redsos_com_ar/_layouts/15/Doc.aspx?sourcedoc=%7B62bf8183-24d5-4395-847e-56b0caaa2879%7D&amp=&action=embedview"> <button type='button' class="btn btn-danger mt-3">Acceso</button></a>
       </div>
     </div>
   );
--- Client/src/Components/Logistica/Tareas/TraficoNacional/Secciones/Flujograma/Flujograma.jsx
+++ Client/src/Components/Logistica/Tareas/TraficoNacional/Secciones/Flujograma/Flujograma.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Flujograma() {
   return (
-    <div className='conteiner-flujograma'>
+    <div className='conteinerFlujograma'>
       <iframe src="https://redsos-my.sharepoint.com/personal/cvanetta_redsos_com_ar/_layouts/15/Doc.aspx?sourcedoc={092f5dfb-e76f-4676-bf31-1455d12f4e50}&amp;action=embedview" width="1280px" height="700px" frameborder="0">Esto es un documento de <a target="_blank" href="https://office.com">Microsoft Office</a> incrustado con tecnología de <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
 
       <div className='mt-3'>
