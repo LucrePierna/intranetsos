81cae06ee7d697cc6c0fc817fd0a476ca5e8c18c
LucasRibotta <lucasribotta15@gmail.com>
2023-09-15 13:43:32 -0300

    update style conteiner

--- Client/src/Components/Logistica/Tareas/Derivacion/secciones/Flujograma/Flujograma.jsx
+++ Client/src/Components/Logistica/Tareas/Derivacion/secciones/Flujograma/Flujograma.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Flujograma() {
   return (
-    <div className='conteiner-flujograma'>
+    <div className='conteinerFlujogramaa'>
       <iframe src="https://redsos-my.sharepoint.com/personal/framirez_redsos_com_ar/_layouts/15/Doc.aspx?sourcedoc={a9380e98-7c70-4f26-a08f-43d8ae3ccd5a}&amp;action=embedview"
         width="1280px" height="700px" frameborder="0">Esto es un documento de <a target="_blank" href="https://office.com" rel="noreferrer">Microsoft Office</a> incrustado con tecnología de <a target="_blank" href="https://office.com/webapps" rel="noreferrer">Office</a>.</iframe>
       <div className='mt-3'>
