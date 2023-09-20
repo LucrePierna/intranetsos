9da17a524e10163eb2db867136bdd60a553d05ad
LucasRibotta <lucasribotta15@gmail.com>
2023-09-20 12:01:46 -0300

    update landing btn

--- Client/src/Components/Landing/Landing.jsx
+++ Client/src/Components/Landing/Landing.jsx
@@ -1,32 +1,27 @@
-import React from 'react';
-import './Landing.css';
+import React from 'react'
+import './Landing.css'
+
 
 export default function Landing() {
   return (
     <div className='contenedor'>
 
-      <h1 className='title'>OPERACIONES SOS</h1>
+      <h2 className='title'>OPERACIONES SOS</h2>
 
-      <div className='row justify-content-center'>
-        <div className='col-lg-3 col-md-4 col-sm-6'>
-          <a href="/cac">
-            <button type="button" className="btn btn-danger mx-2 mb-2 cac">CAC</button>
-          </a>
-        </div>
+      <div className='contenedor-btn'>
+        <a href="/cat">
+          <button type="button" className="btn btn-danger buttonShadow mx-5 ">CAT</button>
+        </a>
 
-        <div className='col-lg-3 col-md-4 col-sm-6'>
-          <a href="/logistica">
-            <button type="button" className="btn btn-danger buttonShadow mx-2 mb-2">RED NACIONAL</button>
-          </a>
-        </div>
+        <a href="/logistica">
+          <button type="button" className="btn btn-danger buttonShadow mx-5 ">RED NACIONAL</button>
+        </a>
 
-        <div className='col-lg-3 col-md-4 col-sm-6'>
-          <a href="/trafico">
-            <button type="button" className="btn btn-danger buttonShadow mx-2 mb-2">TRÁFICO LOCAL</button>
-          </a>
-        </div>
+        <a href="/trafico">
+          <button type="button" className="btn btn-danger buttonShadow mx-5 ">TRÁFICO LOCAL</button>
+        </a>
+
       </div>
-
     </div>
   )
 }
