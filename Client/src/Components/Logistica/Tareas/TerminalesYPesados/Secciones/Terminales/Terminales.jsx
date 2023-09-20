752082cefdc8e08ebf66820ed227801d8f1b6b72
LucasRibotta <lucasribotta15@gmail.com>
2023-09-20 11:31:00 -0300

    update h1 style + estructura img responsive terminales

--- Client/src/Components/Logistica/Tareas/TerminalesYPesados/Secciones/Terminales/Terminales.jsx
+++ Client/src/Components/Logistica/Tareas/TerminalesYPesados/Secciones/Terminales/Terminales.jsx
@@ -39,9 +39,9 @@ export default function Terminales() {
           <p>Son servicios llamados “Especiales”. Aplica a aquellas terminales con las que se haya acordado este tipo de asistencia. La solicitud de asistencia es procesada por el CAT, los operadores SETA, u operadores específicos destinados a tal función (en caso de Mercedes Benz). Los pedidos ingresaran por: Comunicación telefónica o correo electrónico pedido por la Terminal </p>
         </div>
         <div className="tab-pane fade m-auto lineaGeneral" id="nav-general" role="tabpanel" aria-labelledby="nav-general-tab" tabindex="0">
-          <h1><ul>El Operador de Logística deberá buscar un prestador según el tipo de vehículo a asistir:</ul></h1>
+          <h3 className='fs-4'><ul>El Operador de Logística deberá buscar un prestador según el tipo de vehículo a asistir:</ul></h3>
           <p>Moto / Liviano / Liviano Excedido / Liviano Especial / Pesado categoría 1 / Pesado categoría 2 / Pesado categoría 3 / Bus</p>
-          <h1>Aplica a todas aquellas Terminales Automotrices, siendo actualmente:</h1>
+          <h3 className='fs-4'>Aplica a todas aquellas Terminales Automotrices, siendo actualmente:</h3>
           <ul className='list-unstyled text-center justify-content-center'>
             <li>Mercedes Benz (autos, vans , sprinter, camiones) </li>
             <li>Iveco (camiones) </li>
--- Client/src/Components/Logistica/Tareas/TerminalesYPesados/Secciones/TiposDeVehiculos/TipoDeVehiculos.jsx
+++ Client/src/Components/Logistica/Tareas/TerminalesYPesados/Secciones/TiposDeVehiculos/TipoDeVehiculos.jsx
@@ -4,15 +4,15 @@ import tractor from '../../../../Image/tractor.jpg'
 
 export default function TipoDeVehiculos() {
   return (
-    <div className='conteinerNav mt-5 ' >
-      <div className='d-flex flex-row justify-content-evenly m-auto mt-3 px-5'>
-        <div className='px-5 mt-5'>
-          <h3>CHASIS</h3>
-          <img src={chasis} alt="Chasis" height={450} width={700} />
+    <div className='conteinerNav mt-5 rounded py-5' >
+      <div className='d-flex flex-row justify-content-evenly m-auto mt-3 px-5 '>
+        <div className='px-5'>
+          <h3 className='mb-3'><u>CHASIS</u></h3>
+          <img src={chasis} alt="Chasis" className='responsiveImg' />
         </div>
-        <div className='mt-5'>
-          <h3>TRACTOR</h3>
-          <img src={tractor} alt="Eficaz2" height={450} width={700} />
+        <div className='px-5'>
+          <h3 className='mb-3'><u>TRACTOR </u></h3>
+          <img src={tractor} alt="Eficaz2" className='responsiveImg' />
         </div>
       </div>
     </div >
