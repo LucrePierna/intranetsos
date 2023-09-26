26e5921781f9f61191a98b13b81cd4f98845f570
LucasRibotta <lucasribotta15@gmail.com>
2023-09-26 13:27:08 -0300

    config p userButton

--- Client/src/Components/Trafico/Menu/MenuTrafico.jsx
+++ Client/src/Components/Trafico/Menu/MenuTrafico.jsx
@@ -142,7 +142,7 @@ const MenuTrafico = () => {
               Volver a Operaciones
             </a>
           </li>
-          <li className='py-2 px-2'>
+          <li className='p-2 '>
             <UserButton className='bg-white' />
           </li>
         </ul>
