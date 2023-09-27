be2d5130cdfe76c227c3ac944c0954bd0d75d020
LucasRibotta <lucasribotta15@gmail.com>
2023-09-27 12:35:04 -0300

    update rechazo de modalidad

--- Client/src/Components/Logistica/Tareas/Transportin/Secciones/Instructivo/Formato/Formato.jsx
+++ Client/src/Components/Logistica/Tareas/Transportin/Secciones/Instructivo/Formato/Formato.jsx
@@ -90,7 +90,7 @@ export default function Formato() {
         if (formData.aceptaModalidad === 'si') {
             formattedText += 'Acepta Modalidad: Sí\n';
         } else {
-            formattedText += 'Cliente no acepta modalidad.\n';
+            formattedText += 'Se ofrece la modalidad al Cliente, no acepta modalidad. Se le informa que solo cuenta con traslado de zona.\n';
             navigator.clipboard
                 .writeText(formattedText)
                 .then(() => setCopied(true))
