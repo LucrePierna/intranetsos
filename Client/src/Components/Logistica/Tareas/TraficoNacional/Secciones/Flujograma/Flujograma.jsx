c8880a2e82ebcc03df8af46a237d63e0ac304700
LucasRibotta <lucasribotta15@gmail.com>
2023-09-28 11:51:33 -0300

    update flujograma trafico

--- Client/src/Components/Logistica/Tareas/TraficoNacional/Secciones/Flujograma/Flujograma.jsx
+++ Client/src/Components/Logistica/Tareas/TraficoNacional/Secciones/Flujograma/Flujograma.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Flujograma() {
   return (
     <div className='conteinerFlujograma'>
-      <iframe src="https://redsos-my.sharepoint.com/personal/lugomez_redsos_com_ar/_layouts/15/Doc.aspx?sourcedoc={401c6b80-9661-4f3a-b881-20a823da02f7}&amp;action=embedview" width="1200px" height="700px" frameborder="0">Esto es un documento de <a target="_blank" href="https://office.com">Microsoft Office</a> incrustado con tecnología de <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
+      <iframe src="https://redsos.sharepoint.com/_layouts/15/Doc.aspx?sourcedoc={4359f3d0-1145-489f-ad0d-c05e33fb4bfe}&amp;action=embedview" width="1200px" height="700px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> diagram, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
       <div className='mt-3'>
-        <a target='blank' href="https://redsos-my.sharepoint.com/personal/lugomez_redsos_com_ar/_layouts/15/Doc.aspx?sourcedoc={401c6b80-9661-4f3a-b881-20a823da02f7}&amp;action=embedview"> <button type='button' class="btn btn-danger mt-3"> Acceso</button></a>
+        <a target='blank' href="https://redsos.sharepoint.com/_layouts/15/Doc.aspx?sourcedoc={4359f3d0-1145-489f-ad0d-c05e33fb4bfe}&action=embedview"> <button type='button' class="btn btn-danger mt-3"> Acceso</button></a>
       </div>
     </div>
   );
