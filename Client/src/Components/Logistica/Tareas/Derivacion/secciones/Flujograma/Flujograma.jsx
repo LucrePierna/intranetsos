52688dd8ae0776b6cb1dcdee556aaee0e1add41e
LucasRibotta <lucasribotta15@gmail.com>
2023-09-28 11:08:20 -0300

    update link flujograma

--- Client/src/Components/Logistica/Tareas/Derivacion/secciones/Flujograma/Flujograma.jsx
+++ Client/src/Components/Logistica/Tareas/Derivacion/secciones/Flujograma/Flujograma.jsx
@@ -5,7 +5,7 @@ export default function Flujograma() {
     <div className='conteinerFlujograma'>
       <iframe src="https://redsos-my.sharepoint.com/personal/lugomez_redsos_com_ar/_layouts/15/embed.aspx?UniqueId=9dc53577-0c0b-4934-82ec-e456dd3b9d52" width="1200" height="700" frameborder="0" scrolling="no" allowfullscreen title="Derivacion Red Nacional (1).pdf"></iframe>
       <div className='mt-3'>
-        <a target='blank' href="https://redsos-my.sharepoint.com/:u:/g/personal/lugomez_redsos_com_ar/Eb3uHv-qdeBLvMZG8TlvZ2kBA4pla79lYL3TfTsUYTNFaA?e=4%3AgiMpro&fromShare=true&at=9&CID=ae6d9f94-7ebe-3879-58fb-a307126f8af2">
+        <a target='blank' href="https://redsos-my.sharepoint.com/personal/lugomez_redsos_com_ar/_layouts/15/Doc.aspx?sourcedoc={ff1eeebd-75aa-4be0-bcc6-46f1396f6769}&action=embedview">
           <button type='button' class="btn btn-danger mt-3"> Acceso</button></a>
       </div>
     </div>
