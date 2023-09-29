63c4e4020ede5ccbd1d27e43dbe9db114ba3448b
LucasRibotta <lucasribotta15@gmail.com>
2023-09-29 11:22:19 -0300

    loading flujograma

--- Client/src/Components/Logistica/Tareas/Derivacion/secciones/Flujograma/Flujograma.jsx
+++ Client/src/Components/Logistica/Tareas/Derivacion/secciones/Flujograma/Flujograma.jsx
@@ -10,18 +10,12 @@ export default function Flujograma() {
 
   return (
     <div className='conteinerFlujograma'>
-      <div className='loading'> 
-      {!flujogramaCargado && <Spinner label="Cargando..." color="danger" />}
-      </div>
-      {flujogramaCargado && (
         <iframe 
         src="https://redsos.sharepoint.com/_layouts/15/Doc.aspx?sourcedoc={2bf5ff8c-0f15-4093-85a2-25a70faa5db4}&amp;action=embedview" 
         width="1200px" 
         height="700px" 
         frameborder="0"
-        onLoad={handleIframeLoad}
         >This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> diagram, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
-      )}
       <div className='mt-3'>
         <a target='blank' href="https://redsos.sharepoint.com/:u:/r/_layouts/15/doc2.aspx?sourcedoc=%7B2bf5ff8c-0f15-4093-85a2-25a70faa5db4%7D&action=embedview&CID=7677f945-4eba-de08-ce68-cea750573be7">
           <button type='button' class="btn btn-danger mt-3"> Acceso</button></a>
