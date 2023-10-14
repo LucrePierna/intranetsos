cc3c9dfbdef209c56ddd3e4d832641ebcd22598b
LucasRibotta <lucasribotta15@gmail.com>
2023-10-14 10:08:54 -0300

    feat login and fix format transportin

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,8 +2,10 @@ import './App.css'
 import { ClerkProvider } from '@clerk/clerk-react'
 import Login from './Components/Login/Login'
 
+const REACT_APP_CLERK_PUBLISHABLE_KEY= 'pk_test_dW5pdGVkLWNveW90ZS03Ni5jbGVyay5hY2NvdW50cy5kZXYk'
+
 function App () {
- const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY 
+ const clerkPubKey = REACT_APP_CLERK_PUBLISHABLE_KEY 
 
   return (
     <div className='App'>
