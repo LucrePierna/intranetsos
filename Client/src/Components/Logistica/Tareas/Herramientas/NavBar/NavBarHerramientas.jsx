2b11a9bf6ea6de380335fb06f35f2506c758873c
Francisco J. Aguaisol <faguaisol@redsos.com.ar>
2023-10-17 14:48:31 -0300

    Desplazamiento

--- Client/src/Components/Logistica/Tareas/Herramientas/NavBar/NavBarHerramientas.jsx
+++ Client/src/Components/Logistica/Tareas/Herramientas/NavBar/NavBarHerramientas.jsx
@@ -4,6 +4,7 @@ import Calculadoras from '../Secciones/Calculadoras/Calculadoras';
 import Formatos from '../Secciones/Formatos/Formatos';
 import Mapas from '../Secciones/Mapas/Mapas';
 import Links from '../Secciones/Links/Links';
+import Planillas from '../Secciones/Planillas/Planillas';
 
 export default function NavBarHerramientas() {
     const [selectedComponent, setSelectedComponent] = useState('calculadoras');
@@ -55,6 +56,18 @@ export default function NavBarHerramientas() {
                     </li>
                     <li className="nav-item">
                         <button
+                            className={`nav-link btn ${selectedComponent === 'planillas' ? 'active' : ''}`}
+                            onClick={() => handleComponentClick('planillas')}
+                            style={{
+                                color: selectedComponent === 'planillas' ? 'black' : 'red',
+                                backgroundColor: selectedComponent === 'planillas' ? 'white' : 'initial'
+                            }}
+                        >
+                            Planillas
+                        </button>
+                    </li>
+                    <li className="nav-item">
+                        <button
                             className={`nav-link btn ${selectedComponent === 'links' ? 'active' : ''}`}
                             onClick={() => handleComponentClick('links')}
                             style={{
@@ -72,6 +85,7 @@ export default function NavBarHerramientas() {
                 {selectedComponent === 'calculadoras' && <Calculadoras />}
                 {selectedComponent === 'formatos' && <Formatos />}
                 {selectedComponent === 'mapas' && <Mapas />}
+                {selectedComponent === 'planillas' && <Planillas />}
                 {selectedComponent === 'links' && <Links />}
             </div>
         </div>
