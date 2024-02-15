666d777ce4ed4fc2ea9e81c1899df970abd77ff4
Diego <dvidela@redsos.com.ar>
2024-02-15 13:09:29 -0300

    correcciónes

--- Client/src/Components/Trafico/Tareas/Herramientas/Secciones/Calculadoras/Reintegro/Reintegro.jsx
+++ Client/src/Components/Trafico/Tareas/Herramientas/Secciones/Calculadoras/Reintegro/Reintegro.jsx
@@ -179,7 +179,7 @@ function Reintegro() {
                 Compañía:
                 <select className="form-select container-style" value={comp} onChange={(e) => setComp(e.target.value)}>
                   <option value="o">Otras Compañías</option>
-                  <option value="f">Federación Patronal</option>
+                  <option value="f">Federación Patronal y Rivadavia</option>
                   <option value="z">Radios: 120, 121, 137, 138, 227, 278, 816</option>
                 </select>
               </label>
